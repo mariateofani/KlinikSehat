1ec67ff77fdc3c516aa316f55b354eacfc78db09
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-22 17:37:08 +0700

    update

--- api/service/koneksi.php
+++ api/service/koneksi.php
@@ -1,11 +1,26 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "klinik";
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = '4NF19nybZuue1tM.root';
+$pass = 'qv50NP9MXpkJV9DH';
+$db   = 'klinik';
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_init();
 
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
+?>
