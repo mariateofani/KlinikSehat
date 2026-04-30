16b376005a666250ff2295ccd6818e0ffab3900e
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-30 09:45:02 +0700

    update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
-session_unset(); 
-session_destroy();
-header("Location: ../index.html");
+setcookie("email", "", time() - 3600, "/");
+setcookie("nama", "", time() - 3600, "/");
+setcookie("role", "", time() - 3600, "/");
+
+header("Location: login.php");
 exit;
 ?>
