59a98fd745bde7e66b8d9211ff55a2cbf914f115
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-30 11:11:10 +0700

    update editusers.php

--- api/editusers.php
+++ api/editusers.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../service/koneksi.php';
+require_once __DIR__ . '/service/koneksi.php';
 
 // 🔐 CEK LOGIN DARI COOKIE
 if (!isset($_COOKIE['email']) || !isset($_COOKIE['role'])) {
